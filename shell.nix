5c2f4fad081ec83f585b6fdf6c4402bb1ee76e49
Terry Yin <terry.yinzhe@gmail.com>
2024-05-02 12:23:04 +0800

    make apl work

--- shell.nix
+++ shell.nix
@@ -1,25 +1,37 @@
 { pkgs ? import <nixpkgs> {} }:
 
+with pkgs;
+
 pkgs.mkShell {
   buildInputs = [
-    pkgs.python39
-    pkgs.python39Packages.pip
-    pkgs.python39Packages.virtualenv
-    pkgs.jupyter
+    # Defines a python + set of packages.
+    (python3.withPackages (ps: with python3Packages; [
+      jupyter
+      ipython
+      ipykernel
+
+      setuptools
+      virtualenv # run virtualenv .
+      pip
+      pyqt5 # avoid installing via pip
+
+      pandas
+      numpy
+      matplotlib
+      scipy
+    ]))
   ];
 
   shellHook = ''
-    export PYTHONPATH=${pkgs.python39.sitePackages}:$PYTHONPATH
+    export PYTHONPATH=${python3.sitePackages}:$PYTHONPATH
     if [ ! -d .venv ]; then
       python -m venv .venv
-      source .venv/bin/activate
-      pip install ipykernel
-      python -m ipykernel install --user --name=my-nix-project-kernel
       pip install dyalog-jupyter-kernel
-      python -m 'dyalog_kernel' install
-    else
-      source .venv/bin/activate
     fi
+    source .venv/bin/activate
+    export PYTHONPATH=$PWD/.venv/${python3Packages.python.sitePackages}/:$PYTHONPATH
+    python -m ipykernel install --user --name=.venv
+    python -m 'dyalog_kernel' install
     echo "Python and Jupyter are ready to use. Use 'jupyter notebook' to start."
   '';
 }
